c92e701758b2bcfda3f96dd0adaef3a66b6619b9
Kristina Orlichenko <119941674+Kristaal@users.noreply.github.com>
2023-02-14 15:18:04 +0000

    add js for github page and loader

---
+++ assets/js/maps.js
@@ -0,0 +1,22 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var locations = [
+        { lat: 52.27042, lng: -9.70264 },
+        { lat: 52.271823, lng: -9.686548 },
+        { lat: 52.280701, lng: -9.702656 },
+    ];
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
